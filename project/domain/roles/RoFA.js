22ac12cd4fd77f15ef014fe332b6e3b0094b303b
Maor Boublil <boublilm@post.bgu.ac.il>
2021-06-05 17:20:01 +0300

    AddGames working by policy

--- project/domain/roles/RoFA.js
+++ project/domain/roles/RoFA.js
@@ -1,19 +1,67 @@
-function AddGames(season, league){
-    
+const DButils = require("../utils/DButils");
+
+async function AddGames(season, league) {
+  // CHECKS IF LEAGUE NAME EXISTS
+  const LeagueExist = await DButils.execQuery(
+    `SELECT leagueID FROM dbo.sadna_leagues where leagueName='${league}'`
+  );
+  if (LeagueExist.length < 1) {
+    throw { status: 409, message: "League Doesn't Exist in DB" };
+  }
+  let league_id = LeagueExist[0].leagueID;
+
+  // CHECKS IF GAMES ALREADY CREATED
+  const gamesExist = await DButils.execQuery(
+    `SELECT * FROM dbo.sadna_games where League='${league}' and Season='${season}'`
+  );
+  if (gamesExist.length >= 1) {
+    throw { status: 401, message: "Games Already Created" };
+  }
+
+  // Get All league team names
+  const all_teams = await DButils.execQuery(
+    `SELECT teamName FROM dbo.sadna_teams where league='${league_id}'`
+  );
+  let initial_date = new Date();
+  initial_date.setDate(initial_date.getDate() + 7);
+  const referees = await DButils.execQuery(
+    `SELECT user_id FROM dbo.sadna_judges where league='${league_id}' and season='${season}'`
+  );
+  if (referees.length < 2) {
+    throw { status: 409, message: "Not Enough Referees!" };
+  }
+  for (let i = 0; i < all_teams.length; i++) {
+    let home_team = all_teams[i].teamName;
+    for (let j = i; j < all_teams.length; j++) {
+      if (j == i) {
+        continue;
+      }
+      let away_team = all_teams[j].teamName;
+      let location = await DButils.execQuery(
+        `SELECT field FROM dbo.sadna_teams where league='${league_id}' and season='${season}' and teamName='${home_team}'`
+      );
+      await DButils.execQuery(
+        `INSERT INTO dbo.sadna_games (Season, League, HomeTeamName, AwayTeamName, GameDate, Location, MainReferee, SecondaryReferee) VALUES
+                ('${season}','${league}','${home_team}','${away_team}','${initial_date
+          .toISOString()
+          .slice(0, 19)
+          .replace("T", " ")}', '${location[0].field}','${
+          referees[0].user_id
+        }','${referees[1].user_id}')`
+      );
+      initial_date.setDate(initial_date.getDate() + 7);
+    }
+  }
+
+  return true;
 }
 
-function RemoveGame(game){
-    
-}
+function RemoveGame(game) {}
 
-function SetGamePolicy(policy, season, league){
+function SetGamePolicy(policy, season, league) {}
 
-}
+function SetPointsPolicy(policy, season, league) {}
 
-function SetPointsPolicy(policy, season, league){
-    
-}
+function RegisterJudges(judjes, season, league) {}
 
-function RegisterJudges(judjes, season, league){
-
-}
+exports.AddGames = AddGames;
--- project/routes/league.js
+++ project/routes/league.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const league_utils = require("../domain/utils/league_utils");
 const DButils = require("../domain/utils/DButils");
+const RoFA_utils = require("../domain/roles/RoFA");
 
 router.get("/getDetails", async (req, res, next) => {
   try {
@@ -12,7 +13,7 @@ router.get("/getDetails", async (req, res, next) => {
   }
 });
 
-router.post("/addNewGame", async (req, res, next) => {
+router.post("/AddGames", async (req, res, next) => {
   try {
     // only the Representative of the Football Association can add game to a session
     // check the current user is RoAF
@@ -26,87 +27,10 @@ router.post("/addNewGame", async (req, res, next) => {
     if (!isMainRoAF) {
       throw { status: 401, message: "Only RoAF can add a new game" };
     }
-    //session is allways the current one
-    // dont need to check if the tema exsist it's a pre-condition
-    const home_team_name = req.body.homeTeam;
-    const away_team_name = req.body.awayTeam;
-    const date = req.body.date;
-    const location = req.body.location;
-    const season = req.body.season;
-    const league = req.body.league;
-    const mainJudge = req.body.mainJudge;
-    const judge1 = req.body.judge1;
-    const judge2 = req.body.judge2;
-    const judge3 = req.body.judge3;
 
-    //check the teams can play in this date
-    const gameDB = await DButils.execQuery("SELECT * FROM dbo.sadna_games");
-    if (
-      gameDB.find(
-        (x) =>
-          x.GameDate === date &&
-          x.Session === session &&
-          x.HomeTeamName == home_team_name &&
-          x.AwayTeamName == away_team_name
-      )
-    )
-      throw {
-        status: 409,
-        message: "The teams can't play on that date, it's taken",
-      };
-
-    // check the judges exsist
-    let isMainJudge = false;
-    let isJudge1 = false;
-    let isJudge2 = false;
-    let isJudge3 = false;
-    const judgeDB = await DButils.execQuery(
-      "SELECT user_id FROM dbo.sadna_judges"
-    );
-    if (judgeDB.find((x) => x.user_id === mainJudge)) {
-      isMainJudge = true;
-    }
-    if (!isMainJudge) {
-      throw { status: 401, message: "The mainJudge dost exsist" };
-    }
-    if (gameDB.find((x) => x.GameDate === date && x.MainJudge === mainJudge)) {
-      throw { status: 409, message: "The mainJudge can't judge on that date" };
-    }
-
-    if (judgeDB.find((x) => x.user_id === judge1)) {
-      isJudge1 = true;
-    }
-    if (!isJudge1) {
-      throw { status: 401, message: "Judge1 dost exsist" };
-    }
-    if (gameDB.find((x) => x.GameDate === date && x.Judge1 === judge1)) {
-      throw { status: 409, message: "Judge1 can't judge on that date" };
-    }
-
-    if (judgeDB.find((x) => x.user_id === judge2)) {
-      isJudge2 = true;
-    }
-    if (!isJudge2) {
-      throw { status: 401, message: "Judge2 dost exsist" };
-    }
-    if (gameDB.find((x) => x.GameDate === date && x.Judge2 === judge2)) {
-      throw { status: 409, message: "Judge2 can't judge on that date" };
-    }
-
-    if (judgeDB.find((x) => x.user_id === judge3)) {
-      isJudge3 = true;
-    }
-    if (!isJudge3) {
-      throw { status: 401, message: "Judge3 dost exsist" };
-    }
-    if (gameDB.find((x) => x.GameDate === date && x.Judge3 === judge3)) {
-      throw { status: 409, message: "Judge3 can't judge on that date" };
-    }
-
-    // add the new game to Games table
-    await DButils.execQuery(
-      `INSERT INTO dbo.sadna_games (Season, League, HomeTeamName, AwayTeamName,GameDate, Location,MainJudge,Judge1,Judge2,Judge3) VALUES 
-      ('${season}','${league}','${home_team_name}','${away_team_name}','${date}', '${location}','${mainJudge}','${judge1}','${judge2}','${judge3}')`
+    const state = await RoFA_utils.AddGames(
+      req.body.season,
+      req.body.league_name
     );
 
     res.status(201).send("Game Added");
