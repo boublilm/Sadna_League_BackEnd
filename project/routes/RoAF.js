0bd1e4fe7d5702dda2d74adbbf5c0af9fbbc44b6
Maor Boublil <boublilm@post.bgu.ac.il>
2021-06-05 17:45:32 +0300

    updated Referee add on RoAF

--- project/routes/RoAF.js
+++ project/routes/RoAF.js
@@ -2,36 +2,35 @@ var express = require("express");
 var router = express.Router();
 const DButils = require("../domain/utils/DButils");
 
-router.post("/Register", async (req, res, next) => {
+router.post("/RegisterReferee", async (req, res, next) => {
   try {
-    if (req.session.user_id == undefined) {
-      throw { status: 409, message: "User Not Logged In" };
-    } else {
-      const records = await DButils.execQuery(
-        `SELECT * FROM dbo.sadna_RoAF WHERE user_id = '${req.session.user_id}' and league = '${req.body.leagueID}' and season = '${req.body.season}'`
-      );
-      if (records.length >= 1) {
-        throw {
-          status: 409,
-          message:
-            "User is already registered to the league and season as a judge",
-        };
-      }
-      const league = await DButils.execQuery(
-        `SELECT * FROM dbo.sadna_leagues WHERE leagueID = '${req.body.leagueID}'`
-      );
-      if (league.length == 0) {
-        throw {
-          status: 409,
-          message: "League ID doesn't exist",
-        };
-      }
-      // Need To be added season check!
-      await DButils.execQuery(
-        `INSERT INTO dbo.sadna_RoAF (user_id, league, season) VALUES ('${req.session.user_id}', '${req.body.leagueID}', '${req.body.season}')`
-      );
-      res.status(201).send("RoAF Added Successfully");
+    // only the Representative of the Football Association can add game to a session
+    // check the current user is RoAF
+    let isMainRoAF = false;
+    const RoafDB = await DButils.execQuery(
+      "SELECT user_id FROM dbo.sadna_RoAF"
+    );
+    if (RoafDB.find((x) => x.user_id === req.session.user_id)) {
+      isMainRoAF = true;
     }
+    if (!isMainRoAF) {
+      throw { status: 401, message: "Only RoAF can add a new referee" };
+    }
+
+    const records = await DButils.execQuery(
+      `SELECT * FROM dbo.sadna_judges WHERE user_id = '${req.body.user_id}' and league = '${req.body.leagueID}' and season = '${req.body.season}'`
+    );
+    if (records.length >= 1) {
+      throw {
+        status: 409,
+        message:
+          "User is already registered to the league and season as a judge",
+      };
+    }
+    await DButils.execQuery(
+      `INSERT INTO dbo.sadna_judges (user_id, league, season) VALUES ('${req.body.user_id}', '${req.body.leagueID}', '${req.body.season}')`
+    );
+    res.status(201).send("Referee Added Successfully");
   } catch (error) {
     next(error);
   }
