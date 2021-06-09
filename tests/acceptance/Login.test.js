4953f32add9b16f7958e9f9be7deb9ae02048f99
shakedal <salmog510@gmail.com>
2021-06-09 11:46:16 +0300

    revert last changed

--- tests/acceptance/Login.test.js
+++ tests/acceptance/Login.test.js
@@ -1,8 +1,19 @@
 const use_cases = require('./UseCases');
 
+/*
+beforeAll(async () => {
+    await use_cases.LogoutUC();
+});
+
+afterEach(async () => {
+    await use_cases.LogoutUC();
+});
+*/
+
 test('successfull login', async () => {
     expect.assertions(2);
     const response = await use_cases.LoginUC("ladygaga", "lady@56");
+    await use_cases.LogoutUC();
     expect(response.status).toEqual(200);
     expect(response.data).toEqual("login succeeded");
 });
@@ -19,4 +30,13 @@ test('wrong password', async () => {
     const error = (await use_cases.LoginUC("ladygaga", "some_password")).response;
     expect(error.status).toEqual(401);
     expect(error.data).toEqual("Username or Password incorrect");
+});
+
+test('double login', async () => {
+    expect.assertions(2);
+    await use_cases.LoginUC("ladygaga", "lady@56");
+    const error = (await use_cases.LoginUC("ladygaga", "lady@56")).response;
+    await use_cases.LogoutUC();
+    expect(error.status).toEqual(401);
+    expect(error.data).toEqual("User already connected with another device");
 });
