5d857cb1e57283039d232a3439f4951c61834f90
shakedal <salmog510@gmail.com>
2021-06-09 12:51:40 +0300

    save session between tests

--- tests/acceptance/Login.test.js
+++ tests/acceptance/Login.test.js
@@ -1,6 +1,5 @@
 const use_cases = require('./UseCases');
 
-/*
 beforeAll(async () => {
     await use_cases.LogoutUC();
 });
@@ -8,12 +7,11 @@ beforeAll(async () => {
 afterEach(async () => {
     await use_cases.LogoutUC();
 });
-*/
+
 
 test('successfull login', async () => {
     expect.assertions(2);
     const response = await use_cases.LoginUC("ladygaga", "lady@56");
-    await use_cases.LogoutUC();
     expect(response.status).toEqual(200);
     expect(response.data).toEqual("login succeeded");
 });
@@ -36,7 +34,6 @@ test('double login', async () => {
     expect.assertions(2);
     await use_cases.LoginUC("ladygaga", "lady@56");
     const error = (await use_cases.LoginUC("ladygaga", "lady@56")).response;
-    await use_cases.LogoutUC();
     expect(error.status).toEqual(401);
     expect(error.data).toEqual("User already connected with another device");
 });
--- tests/acceptance/UseCases.js
+++ tests/acceptance/UseCases.js
@@ -1,9 +1,19 @@
 const api_domain = "http://localhost:3000";
 const axios = require('axios');
+//make axios work with cookies so we can save session between tests
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const tough = require('tough-cookie');
+const cookieJar = new tough.CookieJar();
+const axios_with_cookies  = axios.create({
+    jar: cookieJar,
+    withCredentials: true
+    });
+axiosCookieJarSupport(axios_with_cookies);
 
 async function LoginUC(username, password){
+
     try{
-        const login_response = await axios.post(`${api_domain}/Login`, {
+        const login_response = await axios_with_cookies.post(`${api_domain}/Login`, {
             username: username,
             password: password
         });
@@ -14,14 +24,16 @@ async function LoginUC(username, password){
 }
 
 async function LogoutUC(){
+    axiosCookieJarSupport(axios);
     try{
-        await axios.post(`${api_domain}/Logout`);
+        await axios_with_cookies.post(`${api_domain}/Logout`);
     } catch(error){
 
     }
 }
 
 async function AssignRefereeUC(usename){
+    axiosCookieJarSupport(axios);
     try{
         const all_users = await axios.get(`${api_domain}/Users`);
         const user = all_users.find(x => x.username == username);
