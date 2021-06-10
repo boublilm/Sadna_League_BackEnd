cd3845e97228fd1cf76d6f06b914cfc48498f072
nicolekenig <49375560+nicolekenig@users.noreply.github.com>
2021-06-10 13:59:06 +0300

    integration testing

--- tests/integration/RoFA.test.js
+++ tests/integration/RoFA.test.js
@@ -1,20 +1,21 @@
 const {test,expect} = require('@jest/globals');
 const {AddGames} = require('../../project/domain/RoFA');
+const user_handler = require('./integrationTestHandler');
+
+const newTimeout = 10000;
+jest.setTimeout(newTimeout);
+
+afterAll(async () =>{
+    // delete users made for tests
+    await user_handler.deleteGames('Spanish La Liga');
+  })
 
 test('test AddGames  GAME CREATED ',async()=>{
-    const ans = await AddGames("2021/2022",'Spanish La Liga');
-    console.log(ans,)
+    const ans = await AddGames("2021/2022",'Spanish La Liga',1);
     expect(ans).toBeUndefined()
-});
+},2000000);
 
 test('test AddGames ERROR GAME CREATED',async()=>{
-    let ans;
-    try{     
-        ans = await AddGames("2021/2022","Premier League");
-        }catch (error){
-        expect(error).toStrictEqual({
-            "message": "Games Already Created",
-             "status": 401
-            })
-    }
+    const ans = await AddGames("2021/2022","Premier League");
+    expect(ans).toBe(-1);
 });
